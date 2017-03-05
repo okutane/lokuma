bb7a739f48d977356a7353af9c904e568df5b3e4
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-05 21:21:57 +0300

    booted by spring

---
+++ src/main/java/ru/urururu/LokumaApplication.java
@@ -0,0 +1,12 @@
+package ru.urururu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokumaApplication.class, args);
+	}
+}
