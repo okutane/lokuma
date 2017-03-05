f860bd2440779af5d445cea7ed53d3f15008aaed
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-06 02:56:46 +0300

    Added prod data source

--- server/src/main/java/ru/urururu/ApiController.java
+++ server/src/main/java/ru/urururu/ApiController.java
@@ -1,8 +1,7 @@
 package ru.urururu;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.urururu.lokuma.reporter.LokumaReport;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -15,9 +14,8 @@ public class ApiController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/api/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    @RequestMapping("/api/publish-report")
+    public void publishReport(@RequestHeader(value = "X-Token") String token, @RequestBody LokumaReport report) {
+
     }
 }
---
+++ server/src/main/java/ru/urururu/LokumaConfig.java
@@ -0,0 +1,31 @@
+package ru.urururu;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author <a href="mailto:dmitriy.g.matveev@gmail.com">Dmitry Matveev</a>
+ */
+@Configuration
+public class LokumaConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
