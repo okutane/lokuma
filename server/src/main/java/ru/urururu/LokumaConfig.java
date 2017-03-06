1cd3b76841549b970cdd0ff4cdbc93b175783214
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-03-06 03:29:19 +0300

    test db connection

--- server/src/main/java/ru/urururu/LokumaConfig.java
+++ server/src/main/java/ru/urururu/LokumaConfig.java
@@ -15,10 +15,11 @@ public class LokumaConfig {
 
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+        URI dbUri = new URI(System.getProperty("CLEARDB_DATABASE_URL"));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
+        String[] dbUriUserInfo = dbUri.getUserInfo().split(":");
+        String username = dbUriUserInfo[0];
+        String password = dbUriUserInfo.length == 2 ? dbUriUserInfo[1] : null;
         String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
 
         BasicDataSource basicDataSource = new BasicDataSource();
--- server/src/test/java/ru/urururu/LokumaApplicationTests.java
+++ server/src/test/java/ru/urururu/LokumaApplicationTests.java
@@ -1,16 +1,23 @@
 package ru.urururu;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LokumaApplicationTests {
+	@Autowired
+	BasicDataSource dataSource;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws SQLException {
+		dataSource.getConnection();
 	}
 
 }
